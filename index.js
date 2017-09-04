947cd8fcd9008d4bc3157e74f4f224ef7116fc1e
Erin Stanfill <erin.stanfill@versionone.com>
2017-09-04 18:35:48 -0400

    #11 don't normalize header row

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ var normalize = require('./lib.js').normalize;
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
+var row = 0;
 process.stdin
 	.pipe(csv.parse())
 	.pipe(csv.transform(function (record) {
+		if (row++ === 0) return record;
 		return normalize(record);
 	}))
 	.pipe(csv.stringify())
